70a48b70902f4374ea0370b11dc6803bffc66afa
Pluto <plutodruid1900@gmail.com>
2018-03-15 21:37:09 +0800

    update

---
+++ account/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+from .import views
+from django.conf import settings
+from django.contrib.auth import views as auth_views
+
+from django.conf import settings
+
+urlpatterns = [
+        #url(r'^login/$', views.user_login,name="user_login"),
+        url(r'^login/$',auth_views.login,name='user_login'),
+        url(r'^logout/$',auth_views.logout,{"template_name":"account/logout.html"},name="user_logout"),
+        url(r'^register/$', views.register,name="user_register"),
+        url(r'^password-change/$',auth_views.password_change,{"post_change_redirect":"/account/password-change-done"},name='password_change'),
+        url(r'^password-change-done/$',auth_views.password_change_done,name='password_change_done'),
+        url(r'^my-information/$', views.myself,name="my_information"),
+        url(r'^edit-my-information/$',views.myself_edit,name="edit_my_information"),
+        url(r'^my-image/$',views.my_image,name="my_image"),
+
+]
---
+++ article/templatetags/article_tags.py
@@ -0,0 +1,27 @@
+from django import template
+register=template.Library()
+from article.models import ArticlePost
+from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
+
+@register.filter(name='markdown')
+def markdown_filter(text):
+    return mark_safe(markdown.markdown(text))
+
+@register.assignment_tag
+def most_commented_articles(n=4):
+    return ArticlePost.objects.annotate(total_comments=Count('comments')).order_by("-total_comments")[:n]
+
+@register.simple_tag
+def total_articles():
+    return ArticlePost.objects.count()
+
+@register.simple_tag
+def author_total_articles(user):
+    return user.article.count()
+
+@register.inclusion_tag('article/list/latest_articles.html')
+def latest_articles(n=5):
+    latest_articles=ArticlePost.objects.order_by("-created")[:n]
+    return {"latest_articles":latest_articles}
